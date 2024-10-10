e672846d7f74241aede5b3061850850c8c571945
WAROL42_c1r5s4 <rabetsyrolio@gmail.com>
2024-10-10 10:41:38 -0400

    mise a jour de fin de journee

--- app/FormDemo.tsx
+++ app/FormDemo.tsx
@@ -1,6 +1,7 @@
 "use client";
 import AutoForm, { AutoFormSubmit } from "@/components/ui/auto-form";
 import { DependencyType } from "@/components/ui/auto-form/types";
+import { DbAutoForm } from "@/prisma-react-component/forms/DbAutoForm";
 import * as z from "zod";
 
 // Define your form schema using zod
@@ -67,85 +68,9 @@ const formSchema = z.object({
 
 export function FormDemo() {
   return (
-    <AutoForm
-      // Pass the schema to the form
-      formSchema={formSchema}
-      // You can add additional config for each field
-      // to customize the UI
-      fieldConfig={{
-        password: {
-          // Use "inputProps" to pass props to the input component
-          // You can use any props that the component accepts
-          inputProps: {
-            type: "password",
-            placeholder: "••••••••",
-          },
-        },
-        favouriteNumber: {
-          // Set a "description" that will be shown below the field
-          description: "Your favourite number between 1 and 10.",
-        },
-        acceptTerms: {
-          inputProps: {
-            required: true,
-          },
-          // You can use JSX in the description
-          description: (
-            <>
-              I agree to the{" "}
-              <a
-                href="#"
-                className="text-primary underline"
-                onClick={(e) => {
-                  e.preventDefault();
-                  alert("Terms and conditions clicked.");
-                }}
-              >
-                terms and conditions
-              </a>
-              .
-            </>
-          ),
-        },
-
-        birthday: {
-          description: "We need your birthday to send you a gift.",
-        },
-
-        sendMeMails: {
-          // Booleans use a checkbox by default, you can use a switch instead
-          fieldType: "switch",
-        },
-      }}
-      // Optionally, define dependencies between fields
-      dependencies={[
-        {
-          // Hide "color" when "sendMeMails" is not checked as we only need to
-          // know the color when we send mails
-          sourceField: "sendMeMails",
-          type: DependencyType.HIDES,
-          targetField: "color",
-          when: (sendMeMails) => !sendMeMails,
-        },
-      ]}
-    >
-      {/* 
-      Pass in a AutoFormSubmit or a button with type="submit".
-      Alternatively, you can not pass a submit button
-      to create auto-saving forms etc.
-      */}
-      <AutoFormSubmit>Send now</AutoFormSubmit>
-
-      {/*
-      All children passed to the form will be rendered below the form.
-      */}
-      <p className="text-gray-500 text-sm">
-        By submitting this form, you agree to our{" "}
-        <a href="#" className="text-primary underline">
-          terms and conditions
-        </a>
-        .
-      </p>
-    </AutoForm>
+    <div>
+      <h3>USER</h3>
+      <DbAutoForm modelName="User" action="create" />
+    </div>
   );
 }
