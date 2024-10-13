42e9673a6b672bfc8c70cb587f98bce6f39751d0
WAROL52 <rabetsyrolio@gmail.com>
2024-10-13 09:16:43 -0400

    mise a becfsefehfjefjk

--- app/FormDemo.tsx
+++ app/FormDemo.tsx
@@ -1,74 +1,15 @@
 "use client";
 import AutoForm, { AutoFormSubmit } from "@/components/ui/auto-form";
 import { DependencyType } from "@/components/ui/auto-form/types";
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { DbAutoForm } from "@/prisma-react-component/forms/DbAutoForm";
 import * as z from "zod";
 
-// Define your form schema using zod
-const formSchema = z.object({
-    nom:z.string(),
-    prenom:z.string().optional(),
-  username: z
-    .string({
-      required_error: "Username is required.",
-    })
-    // You can use zod's built-in validation as normal
-    .min(2, {
-      message: "Username must be at least 2 characters.",
-    }),
-
-  password: z
-    .string({
-      required_error: "Password is required.",
-    })
-    // Use the "describe" method to set the label
-    // If no label is set, the field name will be used
-    // and un-camel-cased
-    .describe("Your secure password")
-    .min(8, {
-      message: "Password must be at least 8 characters.",
-    }),
-
-  favouriteNumber: z.coerce // When using numbers and dates, you must use coerce
-    .number({
-      invalid_type_error: "Favourite number must be a number.",
-    })
-    .min(1, {
-      message: "Favourite number must be at least 1.",
-    })
-    .max(10, {
-      message: "Favourite number must be at most 10.",
-    })
-    .default(5) // You can set a default value
-    .optional(),
-
-  acceptTerms: z
-    .boolean()
-    .describe("Accept terms and conditions.")
-    .refine((value) => value, {
-      message: "You must accept the terms and conditions.",
-      path: ["acceptTerms"],
-    }),
-
-  // Date will show a date picker
-  birthday: z.coerce.date().optional(),
-
-  sendMeMails: z.boolean().optional(),
-
-  // Enum will show a select
-  color: z.enum(["red", "green", "blue"]),
-
-  // Create sub-objects to create accordion sections
-  address: z.object({
-    street: z.string(),
-    city: z.string(),
-    zip: z.string(),
-  }),
-});
 
 export function FormDemo() {
   return (
-    <div>
+    <div className="mx-8">
       <h3>USER</h3>
       <DbAutoForm modelName="User" action="create" />
     </div>
